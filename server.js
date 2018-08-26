9f4bd9918d321cd4c61ee397656c6d16995f52ef
Travis Kane <kevinthomaskane@gmail.com>
2018-08-26 11:08:40 -0700

    add https redirect

--- server.js
+++ server.js
@@ -29,6 +29,18 @@ app.use(
 );
 app.use(bodyParser.json());
 
+app.enable("trust proxy");
+
+app.use(function(req, res, next) {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect("https://" + req.headers.host + req.url);
+  }
+});
+
 app.use(express.static("./public"));
 
 // DB Config
